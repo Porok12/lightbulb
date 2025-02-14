e342324ab6e237a3c531f886dd73f2ee14326d3c
Przemysław Papla <przemekpapla@gmail.com>
2025-02-14 20:38:15 +0100

    configs and libs

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-import * as React from 'react';
-import {AppRegistry, useColorScheme} from 'react-native';
-import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import {AppRegistry} from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/App';
 
-const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        primary: 'tomato',
-        secondary: 'yellow',
-    },
-};
 
-export default function Main() {
-    //const colorScheme = useColorScheme();
-    //const { theme } = useMaterial3Theme();
-
-    return (
-        <PaperProvider theme={theme}>
-            <App />
-        </PaperProvider>
-    );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
